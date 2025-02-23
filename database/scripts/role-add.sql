6e2fd51ff5c959d9807b0ace5572a016bfc0c076
Mr.Jor <62343478+pomeluce@users.noreply.github.com>
2025-02-23 22:14:12 +0800

    feat: Updated database script

--- database/scripts/role-add.sql
+++ database/scripts/role-add.sql
@@ -1,9 +1,9 @@
 insert into akir_role(id, code, name, description, create_by, create_time, update_by, update_time, remark)
-values (akir_snowflake_id(), 'admin', '超级管理员', '管理员角色', (select id from akir_user where account = 'admin'), now(), (select id from akir_user where account = 'admin'),
+values (akir_snowflake_id(), 'admin', '超级管理员', '拥有所有权限', (select id from akir_user where account = 'admin'), now(), (select id from akir_user where account = 'admin'),
         now(),
         '');
 
 insert into akir_role(id, code, name, description, create_by, create_time, update_by, update_time, remark)
-values (akir_snowflake_id(), 'everyone', '所有用户', '默认角色', (select id from akir_user where account = 'admin'), now(), (select id from akir_user where account = 'admin'),
+values (akir_snowflake_id(), 'everyone', '所有用户', '基础功能权限', (select id from akir_user where account = 'admin'), now(), (select id from akir_user where account = 'admin'),
         now(),
         '');
